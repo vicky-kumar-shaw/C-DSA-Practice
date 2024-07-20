e20204172da0b96cd066be87d5cd51fcc0c48448
vicky-kumar-shaw <vicky9073128@gmail.com>
2024-07-20 23:39:23 +0530

    Create numberTrianglePattern.cpp
    triangle pattern using number

---
+++ numberTrianglePattern.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter a number "<<endl;
+    cin>>n;
+    int row=1;
+    while(row<=n){
+        int space=(n-row);
+        while(space){
+            cout<<" ";
+            space=space-1;
+        }
+        int j=1;
+        while (j<=row){
+            cout<<j;
+            j=j+1;
+
+        }
+        int start=row-1;
+        while(start){
+            cout<<start;
+            start=start-1;
+        }
+        cout<<endl;
+        row=row+1;
+    }
+}
